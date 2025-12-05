408d3f3abe548ea1d53e5d1d5e1918ae2ee90f73
lokesh kumar <lokesh800095kumar@gmail.com>
2025-12-05 22:00:19 +0530

    backend

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,18 +2,28 @@ const jwt = require("jsonwebtoken");
 const { errorResponse } = require("../utility/response");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  let authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return errorResponse(res, "No token provided, authorization denied", 401);
+  }
+
+  // Format: "Bearer token_here"
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
 
   if (!token) {
-    return errorResponse(res, "No Token provided, authorization denied");
+    return errorResponse(res, "Token missing or invalid", 401);
   }
+
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_SECRETY_KEY);
     req.user = decoded;
     next();
   } catch (error) {
-    console.log(error);
-    errorResponse(res, "Token is invalid");
+    console.log("JWT ERROR:", error);
+    return errorResponse(res, "Token is invalid", 401);
   }
 };
 

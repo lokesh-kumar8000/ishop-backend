6ac38437ad171d30a31f7ba388881b7e264f9bc4
Lokesh Kumar <151377591+lokesh-kumar8000@users.noreply.github.com>
2025-12-09 15:58:24 +0530

    Replace manual CORS setup with CORS middleware
    Removed manual CORS headers setup in favor of using the CORS middleware.

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ const orderRouter = require("./router/order.router");
 const contactRouter = require("./router/contact.roter");
 const server = express();
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ishop-frontend-hazel.vercel.app"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 server.use(
   cors({
